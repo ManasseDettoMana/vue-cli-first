dfab5f2f0445a60ca44cdbd48685b37976b9b346
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-09-23 16:58:56 +0200

    prova title

---
+++ src/components/title.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Title msg="La mia prima app con Vue CLI"/>
+    </div>
+</template>
+
+<script>
+import Title from './components/title.vue'
+export default {
+    name: 'title',
+    components:{
+        Title
+    }   
+}
+</script>
+
+<style>
+
+</style>
